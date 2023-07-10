93a26cf4672cb82a1a17ef3203cb8d27723d8fe8
DuarteRaphael <raphael-duarte@outlook.com>
2023-07-10 06:32:17 -0300

    Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroClientesComponent } from './cadastro-clientes/cadastro-clientes.component';
+import { ConsultaClientesComponent } from './consulta-clientes/consulta-clientes.component';
+
+/* importando as classes para uso da biblioteca de rotas do Angular
+essas rotas serão utilizadas para a navegação da SPA - Single Page Application
+*/
+import { Routes, RouterModule } from '@angular/router';
+
+//mapear uma rota (URL) para cada componente do projeto.
+const routes : Routes  = [
+  {path : 'cadastro-clientes', component : CadastroClientesComponent },
+  {path : 'consulta-clientes', component : ConsultaClientesComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroClientesComponent,
+    ConsultaClientesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
